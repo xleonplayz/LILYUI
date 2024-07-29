076cc4faa0d074f0ce26b449fd2626d95eaed2a7
muhammadareeb01 <muhammadareeb795@gmail.com>
2024-07-30 01:01:51 +0500

    'homepage-update-1.3'

--- app/start/current_homepage/page.jsx
+++ app/start/current_homepage/page.jsx
@@ -6,6 +6,12 @@ import Footer from '../../../components/Footer';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import backgroundImage from '@/assests/bgimg.png'; // Adjust the path as necessary
+import ibmbg from '@/assests/ibmbg.png';
+import { MdOutlineArrowOutward } from "react-icons/md";
+import Image from 'next/image';
+
+// Import the SVG file from the public directory
+import quantumg from '@/assests/quantum.svg';
 
 const Container = styled.div`
   display: flex;
@@ -57,7 +63,7 @@ const TextContent = styled.div`
   span {
     display: block;
     font-weight: 300;
-      background: linear-gradient(90deg, #9b5cff 0%, #ee5396 100%);
+    background: linear-gradient(90deg, #9b5cff 0%, #ee5396 100%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     width: 100%;
@@ -74,7 +80,7 @@ const ButtonContainer = styled.div`
 const StyledButton = styled.button`
   background-color: ${({ primary }) => (primary ? '#343a3f' : 'transparent')};
   color: ${({ primary }) => (primary ? '#fff' : '#343a3f')};
-  padding: 10px 20px;
+  padding: 13px 20px;
   border: ${({ primary }) => (primary ? 'none' : 'none')};
   cursor: pointer;
   font-size: 1rem;
@@ -88,6 +94,52 @@ const StyledButton = styled.button`
 
   svg {
     margin-left: 5px;
+  }
+`;
+
+const StyledButtonIbm = styled.div`
+  background-color: ${({ primary }) => (primary ? '#343a3f' : 'transparent')};
+  color: ${({ primary }) => (primary ? '#fff' : '#343a3f')};
+  padding: 13px 20px;
+  width: 120px;
+  border: ${({ primary }) => (primary ? 'none' : 'none')};
+  cursor: pointer;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &:hover {
+    background-color: ${({ primary }) => (primary ? '#333' : 'transparent')};
+    color: ${({ primary }) => (primary ? '#fff' : '#343a3f')};
+  }
+
+  svg {
+    margin-left: 5px;
+    font-size: 20px;
+  }
+`;
+
+const StyledButtonQuantum = styled.div`
+  // background-color: ${({ primary }) => (primary ? 'transparent' : 'transparent')};
+  color: ${({ primary }) => (primary ? '#525252' : '#343a3f')};
+  padding: 13px 20px;
+  width: 120px;
+  border:1px solid  ${({ primary }) => (primary ? '#525252' : 'none')};
+  cursor: pointer;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &:hover {
+    background-color: ${({ primary }) => (primary ? '#333' : 'transparent')};
+    color: ${({ primary }) => (primary ? '#fff' : '#343a3f')};
+  }
+
+  svg {
+    margin-left: 5px;
+    font-size: 20px;
   }
 `;
 
@@ -120,9 +172,92 @@ const NewSection = styled.div`
   p {
     font-size: 1rem;
     color: #333;
-    width:30%;
+    width: 30%;
     max-width: 800px;
-    margin: 3%  40%;
+    margin: 3% 40%;
+  }
+`;
+
+const IBMSection = styled.div`
+  display: flex;
+  flex-direction: row; /* Align items in a row */
+  align-items: center; /* Center align items vertically */
+  margin: 10% 0;
+  padding-bottom: 5%;
+  width: 80%;
+  margin: auto;
+  justify-content: space-around;
+
+  .text {
+    width: 50%;
+    // padding-right: 5%; /* Add padding to the right */
+    
+    h2 {
+      font-weight: 200;
+      font-size: 2.625rem;
+      margin: 0;
+      color: #161616;
+    }
+    
+    p {
+      font-size: 1rem;
+      color: #525252;
+      margin: 20px auto;
+    }
+  }
+
+  .image {
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    
+    img {
+      width: auto;
+      height: 100%;
+      max-height: 500px; /* Ensure the image is displayed at its actual height */
+      object-fit: contain;
+    }
+  }
+`;
+
+const QuantumSection = styled.div`  display: flex;
+  flex-direction: row; /* Align items in a row */
+  align-items: center; /* Center align items vertically */
+  margin: 10% 0;
+  padding-bottom: 5%;
+  width: 80%;
+  margin: auto;
+  justify-content: space-around;
+
+
+  .text {
+    width: 40%;
+    
+    h2 {
+      font-weight: 200;
+      font-size: 2.625rem;
+      margin: 0;
+      color: #161616;
+    }
+    
+    p {
+      font-size: 1rem;
+      color: #525252;
+      margin: 20px auto;
+    }
+  }
+
+  .image {
+    width: 50%;
+    // display: flex;
+    // justify-content: center;
+    
+    img {
+      width: auto;
+      height: 100%;
+      max-height: 500px; /* Ensure the image is displayed at its actual height */
+      object-fit: contain;
+    }
   }
 `;
 
@@ -163,10 +298,31 @@ export default function HomePage() {
       </Maincontainer>
       <NewSection>
         <h2>Making the world <span>quantum safe</span></h2>
-        <p>Quantum computers make most of the world’s existing encryption algorithms obsolete. IBM developed many of the foundational technologies that will secure the world in the quantum era, and now offers the tools and services needed to implement them.
-
-        </p>
+        <p>Quantum computers make most of the world’s existing encryption algorithms obsolete. IBM developed many of the foundational technologies that will secure the world in the quantum era, and now offers the tools and services needed to implement them.</p>
       </NewSection>
+
+      <IBMSection>
+        <div className="text">
+          <h2>Start using 127-qubit systems for <br />free</h2>
+          <p>IBM Quantum Platform provides access to systems, documentation, and learning <br />resources all in one place. Sign up today to get 10 free minutes of execution time <br />per month on our utility-scale systems.</p>
+          <StyledButtonIbm primary>Get started <MdOutlineArrowOutward /></StyledButtonIbm>
+        </div>
+        <div className="image">
+          <Image src={ibmbg} alt="IBM Quantum Platform" />
+        </div>
+      </IBMSection>
+
+      <QuantumSection>
+        <div className="image">
+          <Image src={quantumg} alt="Quantum Computing" />
+        </div>
+        <div className="text">
+          <h2>Our commitment to responsible quantum computing</h2>
+          <p>We defined a set of principles to ensure the responsible development, deployment, and<br />use of quantum computing technologies at IBM and beyond.</p>
+          <StyledButtonQuantum primary>Learn More <MdOutlineArrowOutward /></StyledButtonQuantum>
+        </div>
+      </QuantumSection>
+
       <Footer />
     </Container>
   );
